16f18a3a9beba776133f81f171142d59fd8c3725
Edvin Ruiz <edvinruizromero123456@gmail.com>
2026-01-07 17:24:32 -0600

    Simplicity is best, so I made the controller the most simple it can be

--- server-side/src/modules/books/book.controller.ts
+++ server-side/src/modules/books/book.controller.ts
@@ -1,97 +1,80 @@
 import { NextFunction, Response, Request } from "express";
-import {
-  CreateBookInput,
-  UpdateBookInput,
-  DeleteBookInput,
-  GetBookByIdInput,
-} from "./book.inputs.js";
-import {
-  addBookToDatabase,
-  deleteBookAndChapters,
-  getAllBooksByIdFromDB,
-  updateBookInDatabase,
-} from "./book.services.js";
-import getFileLocation from "../../globalServices/getFileLocation.js";
 import getUpdates from "../../globalServices/getUpdates.js";
-import { getAllBooksFromUser } from "./book.services.js";
+import Book from "./book.model.js";
+import { NotFoundError } from "../../errors/notFoundError.js";
+import { deleteBookAndChapters } from "./book.services.js";
 
-const createBook = async (
-  req: Request<any, any, CreateBookInput["body"]>,
-  res: Response,
-  next: NextFunction
-): Promise<any> => {
-  let book: any;
-  try {
-    const userId = req.user!.id;
-    const { title, description } = req.body;
-    const bookData = { userId, title, description };
-    book = await addBookToDatabase(bookData);
-
-    const location = getFileLocation(req.originalUrl, book._id);
-    req.responseData = { location, status: 201, body: book };
-
-    next();
-  } catch (error: any) {
-    return next(error);
-  }
-};
-
-const updateBook = async (
-  req: Request<UpdateBookInput["params"], any, UpdateBookInput["body"]>,
-  res: Response,
-  next: NextFunction
-): Promise<any> => {
-  let book: any;
-  try {
-    const { bookId } = req.params;
-    const updates = getUpdates(req.body);
-    book = updateBookInDatabase(bookId, updates);
-
-    const location = getFileLocation(req.originalUrl, book._id);
-    req.responseData = { location, status: 201, body: book };
-
-    next();
-  } catch (error: any) {
-    return next(error);
-  }
-};
-
-const getAllBooks = async (
+export const createBook = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  let books = {};
   try {
-    const userId = req.user!.id;
-    books = getAllBooksFromUser(userId);
+    const book = new Book();
+    book.title = req.body.title;
+    book.description = req.body.description;
+    book.author = req.user!.id;
+    await book.save();
 
-    req.responseData = { body: books, status: 200 };
-    next();
+    const location = req.originalUrl + book._id;
+    return res.status(201).location(location).json(book);
   } catch (error: any) {
-    next(error);
+    return next(error);
   }
 };
 
-const getBookById = async (
-  req: Request<GetBookByIdInput["params"], any, any>,
+export const updateBook = async (
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  let book = {};
   try {
     const { bookId } = req.params;
-    book = getAllBooksByIdFromDB(bookId);
+    const updates = getUpdates(req.body);
+    const book = await Book.findByIdAndUpdate(bookId, updates);
+    if (!book) throw new NotFoundError();
 
-    req.responseData = { body: book, status: 200 };
-    next();
+    const location = req.originalUrl + book._id;
+    return res.status(200).location(location).json(book);
+  } catch (error: any) {
+    return next(error);
+  }
+};
+
+export const getAllBooks = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const userId = req.user!.id;
+    const books = await Book.find({ author: userId });
+    if (!books) throw new NotFoundError();
+
+    return res.status(200).json(books);
   } catch (error: any) {
     next(error);
   }
 };
 
-const deleteBook = async (
-  req: Request<DeleteBookInput["params"], any, any>,
+export const getBookById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { bookId } = req.params;
+    const book = await Book.findById(bookId);
+    if (!book) throw new NotFoundError();
+
+    return res.status(200).json(book);
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const deleteBook = async (
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
@@ -99,20 +82,8 @@ const deleteBook = async (
     const { bookId } = req.params;
     await deleteBookAndChapters(bookId);
 
-    req.responseData = {
-      body: { message: "Succesfully deleted" },
-      status: 200,
-    };
-    next();
+    return res.status(200).json({ message: "Succesfully deleted" });
   } catch (error: any) {
     next(error);
   }
-};
-
-export default {
-  createBook,
-  getBookById,
-  deleteBook,
-  getAllBooks,
-  updateBook,
 };
