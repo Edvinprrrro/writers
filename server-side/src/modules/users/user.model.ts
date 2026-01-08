47a7e89d5f6d961e78ca230432b066e8e083e695
Edvin Ruiz <edvinruizromero123456@gmail.com>
2026-01-07 20:21:45 -0600

    Added the class to the user model, which gives, setPassword, generateJwt and isPasswordValid

--- server-side/src/modules/users/user.model.ts
+++ server-side/src/modules/users/user.model.ts
@@ -1,5 +1,16 @@
-import mongoose, { Schema, InferSchemaType } from "mongoose";
-import { Document } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import * as crypto from "crypto";
+import * as jwt from "jsonwebtoken";
+
+interface User extends mongoose.Document {
+  email: string;
+  username: string;
+  hash: string;
+  salt: string;
+  setPassword(password: string): void;
+  generateJwt(): { token: string; expiry: Date };
+  isPasswordValid(password: string): boolean;
+}
 
 // Now create the actual user schema
 const userSchema = new Schema(
@@ -14,7 +25,11 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
-    passwordHash: {
+    hash: {
+      type: String,
+      required: true,
+    },
+    salt: {
       type: String,
       required: true,
     },
@@ -22,10 +37,43 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-// Creating the interface for the user model
-export type UserDocument = Document & InferSchemaType<typeof userSchema>;
+class UserClass {
+  private _id!: string;
+  private email!: string;
+  public username!: string;
+  private salt!: string;
+  private hash!: string;
 
-// Now create the user model
-const UserModel = mongoose.model<UserDocument>("User", userSchema);
+  public setPassword(password: string) {
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto
+      .pbkdf2Sync(password, this.salt, 100000, 512, "sha512")
+      .toString("hex");
+  }
 
-export default UserModel;
+  public generateJwt(): { token: string; expiry: Date } {
+    const expiry = new Date();
+    expiry.setHours(expiry.getHours() + 2);
+
+    const token = jwt.sign(
+      {
+        _is: this._id,
+        email: this.email,
+        exp: Math.round(expiry.getTime() / 1000),
+      },
+      process.env.JWT_ACCESS_KEY!
+    );
+
+    return { token, expiry };
+  }
+
+  public isPasswordValid(password: string) {
+    const hash = crypto
+      .pbkdf2Sync(password, this.salt, 100000, 512, "sha512")
+      .toString("hex");
+    return hash === this.hash;
+  }
+}
+
+userSchema.loadClass(UserClass);
+export default mongoose.model<User>("User", userSchema);
