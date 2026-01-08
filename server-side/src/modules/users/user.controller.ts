1ccf68cd3a65b225fefbfcf66efdb7ecd481f60c
Edvin Ruiz <edvinruizromero123456@gmail.com>
2026-01-07 20:22:26 -0600

    Improved the login and register functions in the controller

--- server-side/src/modules/users/user.controller.ts
+++ server-side/src/modules/users/user.controller.ts
@@ -1,99 +1,63 @@
 import { NextFunction, Request, Response } from "express";
-import bcrypt from "bcrypt";
-import User, { IUser } from "./user.model";
-import { ILoginData, IRegisterData } from "./user.types";
-import { HydratedDocument } from "mongoose";
-import { AuthRequest } from "../../middleware/authenticateAccessToken";
-import Book from "../books/book.model";
+import User from "./user.model";
 import Chapter from "../chapters/chapter.model";
 import Character from "../characters/character.model";
 import PlotPoint from "../plotPoints/plotPoint.model";
-
-interface RegisterUserRequest extends AuthRequest {
-  body: IRegisterData;
-}
+import { HttpError } from "../../errors/httpError";
+import { NotFoundError } from "../../errors/notFoundError";
 
 export const registerUser = async (
-  req: RegisterUserRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { email, username, password, repeatedPassword } = req.body;
-  // Check if all the fields are sent
-  if (!email || !username || !password || !repeatedPassword) {
-    return res
-      .status(400)
-      .json({ error: "Missing fields to register a valid user" });
+  try {
+    const { password, repeatedPassword, email, username } = req.body;
+
+    if (password === repeatedPassword)
+      throw new HttpError(400, "Passwords do not match");
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) throw new HttpError(400, "Email already being used");
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) throw new HttpError(400, "Username is taken");
+
+    const user = new User();
+    user.email = email;
+    user.username = username;
+    user.setPassword(password);
+    await user.save();
+
+    const tokenSignature = user.generateJwt();
+    return res.status(201).json(tokenSignature);
+  } catch (error: any) {
+    next(error);
   }
-
-  // Check if the repeated password matches the password
-  if (password !== repeatedPassword)
-    return res.status(400).json({ error: "Passwords do not match" });
-
-  // Check if the email is already being used
-  const existingEmail = await User.findOne({ email: email });
-  if (existingEmail != null) {
-    return res.status(400).json({ error: "Email already being used" });
-  }
-
-  // Check if the username is taken
-  const existingUsername = await User.findOne({ username: username });
-  if (existingUsername != null) {
-    return res.status(400).json({ error: "Username already taken" });
-  }
-
-  // Encrypt the password
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
-
-  const user = new User({
-    username,
-    email,
-    passwordHash,
-  });
-
-  await user.save();
-
-  req.user = user;
-  next();
 };
 
-interface LoginUserRequest extends AuthRequest {
-  body: ILoginData;
-}
-
 export const loginUser = async (
-  req: LoginUserRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { password, username } = req.body;
+  try {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username });
 
-  // Check if all fields are sent
-  if (!password || !username) {
-    return res.status(400).json({ error: "Missing fields" });
+    if (!user) throw new NotFoundError();
+    if (!user.isPasswordValid(password))
+      throw new HttpError(401, "Incorrect password");
+
+    const tokenSignature = user.generateJwt();
+    return res.status(200).json(tokenSignature);
+  } catch (error: any) {
+    next(error);
   }
-
-  // Check if user exists by that username
-  const user: HydratedDocument<IUser> | null = await User.findOne({ username });
-  if (!user) {
-    return res
-      .status(400)
-      .json({ error: "User not found, incorrect username" });
-  }
-
-  // Check if password is correct
-  const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
-  if (!isPasswordCorrect) {
-    return res.status(400).json({ error: "Incorrect paswword" });
-  }
-
-  req.user = user;
-  next();
 };
 
 export const deleteUser = async (
-  req: AuthRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
