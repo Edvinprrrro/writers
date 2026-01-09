dc8bbaf7c2f0191974774ee6c8bb8c71748e44cb
Edvin Ruiz <edvinruizromero123456@gmail.com>
2026-01-08 20:55:10 -0600

    Made the character controller functions all more simple

--- server-side/src/modules/characters/character.controller.ts
+++ server-side/src/modules/characters/character.controller.ts
@@ -1,105 +1,92 @@
 import { NextFunction, Response, Request } from "express";
 import Character from "./character.model.js";
-import { CreateCharacterDto, UpdateCharacterDto } from "./character.dto.js";
+import { NotFoundError } from "../../errors/notFoundError.js";
+import { HttpError } from "../../errors/httpError.js";
 
 export const getAllCharacters = async (
-  req: Request<{ bookId: string }, any, any>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { bookId } = req.params;
+  try {
+    const { bookId } = req.params;
+    const characters = await Character.find({ books: [bookId] });
+    if (!characters) throw new NotFoundError();
 
-  const characters = await Character.find({ books: bookId });
-  return res.status(200).json(characters);
+    return res.status(200).json(characters);
+  } catch (error: any) {
+    return next(error);
+  }
 };
 
 export const getCharacterById = async (
-  req: Request<{ bookId: string; characterId: string }, any, any>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { characterId, bookId } = req.params;
-  if (!characterId)
-    return res.status(400).json({ error: "No character id is provided" });
+  try {
+    const { characterId } = req.params;
+    const character = await Character.findById(characterId);
+    if (!character) throw new NotFoundError();
 
-  const character = await Character.findById(characterId);
-  if (!character) return res.status(404).json({ error: "Character not found" });
-
-  return res.status(200).json(character);
+    return res.status(200).json(character);
+  } catch (error: any) {
+    return next(error);
+  }
 };
 
 export const createCharacter = async (
-  req: Request<{ bookId: string }, any, CreateCharacterDto>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { name, description, narrativeRole } = req.body;
-  console.log("Got to create character");
-  if (!name || !description || !narrativeRole)
-    return res
-      .status(400)
-      .json({ error: "The body does not have all the requred fields" });
+  try {
+    const character = new Character();
+    character.name = req.body.name;
+    character.description = req.body.description;
+    character.narrativeRole = req.body.narrativeRole;
+    character.books.push(req.params.bookId);
+    await character.save();
 
-  const { bookId } = req.params;
-
-  const character = await Character.create({
-    name,
-    description,
-    narrativeRole,
-    books: [bookId],
-  });
-  if (!character)
-    return res.status(500).json({ error: "Failed to create the character" });
-
-  console.log("Basically go to the response");
-
-  return res
-    .status(201)
-    .location(`/books/${bookId}/characters/${character._id}`)
-    .json(character);
+    const location = req.originalUrl + character._id;
+    return res.status(201).location(location).json(character);
+  } catch (error: any) {
+    return next(error);
+  }
 };
 
 export const updateCharacter = async (
-  req: Request<any, any, UpdateCharacterDto>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { id, bookId } = req.params;
-  if (!id) return res.status(400).json({ error: "No character id provided" });
+  try {
+    const { characterId } = req.params;
+    const updates = req.body;
+    if (!updates) throw new HttpError(400, "No updates were sent");
 
-  const { name, description, narrativeRole } = req.body;
-  if (!name && !description && !narrativeRole)
-    return res.status(400).json({ error: "Not a single change was sent" });
+    const character = await Character.findByIdAndUpdate(characterId, updates);
+    if (!character) throw new NotFoundError();
 
-  // Check updates to be made
-  const updates: UpdateCharacterDto = {};
-  Object.entries(req.body).forEach(([key, value]) => {
-    if (value !== undefined) (updates as any)[key] = value;
-  });
-
-  const character = await Character.findByIdAndUpdate(id, updates);
-  if (!character)
-    return res
-      .status(500)
-      .json({ error: "An error updating the character ocurred" });
-
-  return res
-    .status(200)
-    .location(`/books/${bookId}/characters/${id}`)
-    .json(character);
+    const location = req.originalUrl + character._id;
+    return res.status(200).location(location).json(character);
+  } catch (error: any) {
+    return next(error);
+  }
 };
 
 export const deleteCharacter = async (
-  req: Request<{ characterId: string }, any, any>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
-  const { characterId } = req.params;
-  if (!characterId)
-    return res.status(400).json({ error: "No character id provided" });
+  try {
+    const { characterId } = req.params;
+    const character = await Character.findByIdAndDelete(characterId);
+    if (!character) throw new NotFoundError();
 
-  const character = await Character.findByIdAndDelete(characterId);
-  if (!character) return res.status(404).json({ error: "Character not found" });
-
-  return res.status(200).json(character);
+    return res.status(200).json(character);
+  } catch (error: any) {
+    return next(error);
+  }
 };
